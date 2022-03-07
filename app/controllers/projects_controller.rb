602e58e859d2a8e76c9396ff524131ab6f318c62
Ademola <ademolaogunmokun@yahoo.com>
2022-03-07 01:08:14 +0100

    action text

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-
+  
   # GET /projects or /projects.json
   def index
     @projects = Project.all
@@ -65,6 +65,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :donation_goal, :user_id)
+      params.require(:project).permit(:title, :donation_goal, :description)
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def author_of(resource)
+    user_signed_in && current_user.id == resource.user_id
+  end
 end
