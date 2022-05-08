455bac0c389b84a9c0a730676fd9184fb092f13d
Ademola <ademolaogunmokun@yahoo.com>
2022-05-08 13:55:49 +0100

    fixed the project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
   
   # GET /projects or /projects.json
   def index
@@ -22,11 +23,12 @@ class ProjectsController < ApplicationController
   # POST /projects or /projects.json
   def create
     @project = Project.new(project_params)
+    @project.user_id = current_user.id
+    
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
-        format.json { render :show, status: :created, location: @project }
+        render  "projects/index"
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @project.errors, status: :unprocessable_entity }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def author_of(resource)
-    user_signed_in && current_user.id == resource.user_id
+    user_signed_in? && current_user.id == resource.user_id
+  end
+
+  def admin?
+    user_signed_in? && current_user.admin?
   end
 end
